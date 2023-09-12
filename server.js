0465db6772ae7361c3bd3354f9e11869f8746712
Taher Moosa <lilt@Tahers-MacBook-Pro.local>
2023-09-12 18:59:38 -0500

    lisen port

--- server.js
+++ server.js
@@ -4,3 +4,45 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const Flight = require('./models/flight')
 const mongoose = require('mongoose')
+
+
+
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+mongoose.connection.once("open", () => {
+    console.log("connected to mongo")
+})
+
+const jsxViewEngine = require('jsx-view-engine');
+app.set('view engine', 'jsx');
+app.engine('jsx', jsxViewEngine());
+
+app.use((req, res, next) => {
+    console.log(("Middleware: I run for all routes"));
+    next();
+});
+app.use(express.urlencoded({ extended: false }))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(PORT, () => {
+    console.log(`Listening on port: ${PORT}`);
+});
